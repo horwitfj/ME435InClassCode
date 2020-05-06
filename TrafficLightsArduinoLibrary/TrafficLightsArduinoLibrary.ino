09a6d179bcb66e7ad4306218d73d73153639ef20
horwitfj <64371006+horwitfj@users.noreply.github.com>
2020-05-05 21:55:26 -0400

    AutoFormat Test

--- TrafficLightsArduinoLibrary/TrafficLightsArduinoLibrary.ino
+++ TrafficLightsArduinoLibrary/TrafficLightsArduinoLibrary.ino
@@ -1,42 +1,36 @@
-#define PIN_LED_RED  			10 
-#define PIN_LED_YELLOW  		9
-#define PIN_LED_GREEN  			6
-#define PIN_LED_BLUE  			5
+#define PIN_LED_RED 10
+#define PIN_LED_YELLOW 9
+#define PIN_LED_GREEN 6
+#define PIN_LED_BLUE 5
 
-#define PRESSED  	0
-#define UNPRESSED  	1
+#define PRESSED 0
+#define UNPRESSED 1
 
-#define PIN_PUSHBUTTON_BLUE  	0
-
+#define PIN_PUSHBUTTON_BLUE 0
 
 int pushed = 0;
 
-void setup()
-{
+void setup() {
   Serial.begin(9600);
   pinMode(PIN_LED_RED, OUTPUT);
   pinMode(PIN_LED_YELLOW, OUTPUT);
   pinMode(PIN_LED_GREEN, OUTPUT);
-  
+
   pinMode(PIN_PUSHBUTTON_BLUE, INPUT_PULLUP);
 }
 
-void loop()
-{
+void loop() {
   if (digitalRead(PIN_PUSHBUTTON_BLUE) == LOW) {
-      
-  	digitalWrite(PIN_LED_GREEN, HIGH);
+    digitalWrite(PIN_LED_GREEN, HIGH);
     pushed = 1;
-  	delay(1000);
-  } 
-  if ((pushed == 1) && (digitalRead(PIN_PUSHBUTTON_BLUE) == HIGH)){
-      digitalWrite(PIN_LED_GREEN, LOW);
-      digitalWrite(PIN_LED_YELLOW, HIGH);
-      delay(2000);
-      digitalWrite(PIN_LED_RED, HIGH);
-      digitalWrite(PIN_LED_YELLOW, LOW);
+    delay(1000);
+  }
+  if ((pushed == 1) && (digitalRead(PIN_PUSHBUTTON_BLUE) == HIGH)) {
+    digitalWrite(PIN_LED_GREEN, LOW);
+    digitalWrite(PIN_LED_YELLOW, HIGH);
+    delay(2000);
+    digitalWrite(PIN_LED_RED, HIGH);
+    digitalWrite(PIN_LED_YELLOW, LOW);
     pushed = 0;
   }
-  
-   
 }
--- TrafficLightsWithSFRs/TrafficLightsWithSFRs.ino
+++ TrafficLightsWithSFRs/TrafficLightsWithSFRs.ino
@@ -1,46 +1,39 @@
-#define PIN_LED_RED  			10 
-#define PIN_LED_YELLOW  		9
-#define PIN_LED_GREEN  			6
-#define PIN_LED_BLUE  			5
+#define PIN_LED_RED 10
+#define PIN_LED_YELLOW 9
+#define PIN_LED_GREEN 6
+#define PIN_LED_BLUE 5
 
-#define PRESSED  	0
-#define UNPRESSED  	1
+#define PRESSED 0
+#define UNPRESSED 1
 
-#define PIN_PUSHBUTTON_BLUE  	0
-
+#define PIN_PUSHBUTTON_BLUE 0
 
 int pushed = 0;
 
-void setup()
-{
-
+void setup() {
   Serial.begin(9600);
   DDRD = B01000000;
   DDRB = B00000110;
-  
-  PORTD= B00000001;
+
+  PORTD = B00000001;
 }
 
-void loop()
-{
+void loop() {
   int btn = (PIND & _BV(PIN_PUSHBUTTON_BLUE)) >> PIN_PUSHBUTTON_BLUE;
 
-
   if (btn == LOW) {
-      PORTD = _BV(PIN_LED_GREEN) | PORTD;
+    PORTD = _BV(PIN_LED_GREEN) | PORTD;
     pushed = 1;
-  	delay(1000);
-  } 
-  if ((pushed == 1) && (btn == HIGH)){
-      PORTD = ~_BV(PIN_LED_GREEN) | PORTD;
-      PORTB =  _BV(PIN_LED_YELLOW) | PORTB;
-        
-      delay(2000);
-      
-      PORTB = ~_BV(PIN_LED_YELLOW) | PORTB;
-      PORTB =  _BV(PIN_LED_RED) | PORTB;
+    delay(1000);
+  }
+  if ((pushed == 1) && (btn == HIGH)) {
+    PORTD = ~_BV(PIN_LED_GREEN) | PORTD;
+    PORTB = _BV(PIN_LED_YELLOW) | PORTB;
+
+    delay(2000);
+
+    PORTB = ~_BV(PIN_LED_YELLOW) | PORTB;
+    PORTB = _BV(PIN_LED_RED) | PORTB;
     pushed = 0;
   }
-  
-   
 }
